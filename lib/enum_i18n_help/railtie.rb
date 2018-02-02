42e437ef400642c6398977098c8f682a40cfe6ba
ShuheiTsuji <shuhei.tsuji@icloud.com>
2018-02-02 14:52:11 +0900

    delete lib/enum_i18n_help/railtie.rb

--- lib/enum_i18n_help/railtie.rb
+++
@@ -1,9 +0,0 @@
-module EnumI18nHelp
-  class Railtie < Rails::Railtie
-    initializer "enum_i18n_help.i18n" do
-      ActiveSupport.on_load(:active_record) do
-        extend EnumI18nHelp::EnumI18n
-      end
-    end
-  end
-end
